758be779c7432de758d987c28f819045c3580405
Ron Maina <ronmwangi00@gmail.com>
2024-02-07 17:04:24 +0300

    input models for testing server

--- app/api_models.py
+++ app/api_models.py
@@ -1,8 +1,14 @@
 from flask_restx import fields
 from .extensions import api
 
-user_input_model = api.model("UserInput", {
+user_registration_model = api.model("Signup", {
     "username": fields.String,
     "email": fields.String,
     "password": fields.String,
 })
+
+user_login_model = api.model("Login", {
+    "username": fields.String,
+    "password": fields.String,
+})
+
