29346a170c808c3e9a486e266854ea31ba62c7be
Ron Maina <ronmwangi00@gmail.com>
2024-02-09 13:33:08 +0300

    Added token expiration time

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,32 @@
 from flask import Flask
 
 from flask_migrate import Migrate
+from flask_cors import CORS
+
 from app import models
-from .extensions import api, db
+from .extensions import api, db, jwt
 from .routes import ns
+
+from datetime import timedelta
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///not_netflix.db'
+    app.config["JWT_SECRET_KEY"] = "91a199475acfdabb2ba1e513e0b07e38" 
+    app.config["JWT_ALGORITHM"] = "HS256"
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=15)  
+    app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=1)  
+
+
 
     api.init_app(app)
     db.init_app(app)
-
+    jwt.init_app(app)
+    
     migrate = Migrate(app, db)
+
+    cors = CORS(app)
 
     api.add_namespace(ns)
 
